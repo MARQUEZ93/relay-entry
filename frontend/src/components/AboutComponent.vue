a5b03ae4d74f26d4e445442b34e7b42765095024
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2024-08-10 17:27:48 -0500

    make progress on about page

--- frontend/src/components/AboutComponent.vue
+++ frontend/src/components/AboutComponent.vue
@@ -14,14 +14,13 @@ export default {
     <v-container>
         <v-row>
             <v-col cols="12">
-                <h1>About RelayEntry</h1>
+                <v-typography class="h1-about" variant="h1">About RelayEntry</v-typography>
                 <v-sheet class="mx-auto my-5 pa-5" elevation="2">
                     <v-row>
                         <v-col cols="12">
-                            <h2>Our Mission</h2>
-                            <p>RelayEntry was created to make it easier for anyone to start their own race.</p>
-                            <p>We are committed to providing the best racing platform that caters to the needs of race organizers, racers and spectators alike.</p>
-                            <p>Currently, our product supports team relay races, with plans to expand to other race types in Q4 2024.</p>
+                            <v-typography class="body-about" variant="body">RelayEntry was created to make it easier for anyone to start their own race. </v-typography>
+                            <v-typography class="body-about" variant="body">We are committed to providing the best racing platform that caters to the needs of race organizers, racers and spectators alike. </v-typography>
+                            <v-typography class="body-about" variant="body">Currently, our product supports team relay races, with plans to expand to other race types in Q4 2024.</v-typography>
                         </v-col>
                     </v-row>
                 </v-sheet>
@@ -31,59 +30,56 @@ export default {
 </template>
 
 <style scoped>
-h1 {
-color: #2c3e50;
-margin-top: 20px;
-text-align: center;
-}
+    .h1-about {
+        color: #4caf50;
+        margin-top: 20px;
+        font-size: 3rem;
+        text-align: center;
+        margin-bottom: 10px;
+    }
 
-p {
-font-size: 1.2em;
-color: #2c3e50;
-text-align: center;
-}
+    .body-about {
+        font-size: 1.2em;
+        color: #2c3e50;
+        text-align: center;
+    }
 
-h2 {
-color: #4caf50;
-margin-bottom: 10px;
-}
+    .v-divider {
+        margin-top: 20px;
+    }
 
-.v-divider {
-margin-top: 20px;
-}
+    .features-list {
+        list-style-type: disc;
+        padding-left: 20px;
+    }
 
-.features-list {
-list-style-type: disc;
-padding-left: 20px;
-}
+    .features-list li {
+        text-align: left;
+        display: flex;
+        align-items: center;
+        margin-bottom: 10px;
+        font-size: 1.1em;
+        word-wrap: break-word;
+        white-space: normal;
+    }
 
-.features-list li {
-text-align: left;
-display: flex;
-align-items: center;
-margin-bottom: 10px;
-font-size: 1.1em;
-word-wrap: break-word;
-white-space: normal;
-}
+    .features-list li v-icon {
+        margin-right: 8px;
+        color: #4caf50;
+    }
 
-.features-list li v-icon {
-margin-right: 8px;
-color: #4caf50;
-}
+    @media (max-width: 600px) {
+        h1, h2 {
+            font-size: 1.5em;
+        }
 
-@media (max-width: 600px) {
-h1, h2 {
-    font-size: 1.5em;
-}
+        p {
+            font-size: 1em;
+        }
 
-p {
-    font-size: 1em;
-}
-
-.features-list li {
-    font-size: 1em;
-}
-}
+        .features-list li {
+            font-size: 1em;
+        }
+    }
 </style>
   
